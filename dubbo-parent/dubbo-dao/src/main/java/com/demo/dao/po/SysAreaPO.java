25724e464a83c24548fa1638a19faa99ba49f41a
Minsky <mingorun@163.com>
2019-05-28 11:46:38 +0800

    增加mybatis 二级缓存

--- dubbo-parent/dubbo-dao/src/main/java/com/demo/dao/po/SysAreaPO.java
+++ dubbo-parent/dubbo-dao/src/main/java/com/demo/dao/po/SysAreaPO.java
@@ -2,8 +2,15 @@ package com.demo.dao.po;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
+/**
+ * 省市区三级联动表由于数据几乎无需更新,因此单表查询结果开启mybatis 二级缓存
+ * 二级缓存会将查询结果映射的java对象保存在 一个map中,
+ * 因此需要实现 Serializable 接口
+ */
 @Data
-public class SysAreaPO {
+public class SysAreaPO implements Serializable {
     private Integer id;
     private String province;
     private String city;
