4bfa96ebcd04e24bbbb262bd8b2703bcf98e35e6
Gavin16 <mingorun@163.com>
2019-06-15 23:21:42 +0800

    多人开发，测试提交

--- dubbo-springboot-consumer/src/main/java/code/BootConsumerApplication.java
+++ dubbo-springboot-consumer/src/main/java/code/BootConsumerApplication.java
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Auther: Minsky
  * @Date: 2019/2/17 19:30
  * @Description:
+ *
+ * 项目克隆下来之后,如果在多个窗口打开可能遇到
+ * (1)各包所在目录不是git根目录的问题, 只需要在event log 中add root即可
+ * (2) 打开项目是可能看不到源码,可以在右边侧边 maven projects 栏中  点击"Generata Sources and Update Folders For All Projects" 即可
  */
 @EnableDubbo
 @SpringBootApplication
